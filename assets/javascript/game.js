4ec7986cf77de4f92c7a966fdf4fe3f2567e18be
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-11-10 08:10:43 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,24 +16,24 @@ console.log(randomWord);
 var rightWord = [];
 var wrongWord = [];
 
+// DOM
+var domUnderscore = document.getElementsByClassName('underscore');
+
 // underscore generation
 var underScore = [];
-var generateUnderscore = () => {
+var generateUnderscore = function() {
     for(var i = 0; i < randomWord.length; i++)
         underScore.push("_");
-        domUnderscore[0].innerHTML = underscore.join('');
+        domUnderscore.innerHTML = underScore.join('');
 
         return underScore;
 }
 console.log(generateUnderscore());
 
- // DOM
- var domUnderscore = document.getElementsByClassName('underScore');
-
 
 
  // record user entry
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keypress', function() {
         var keyCode = event.keyCode;
         var keyWord = String.fromCharCode(keyCode);
 
@@ -42,10 +42,12 @@ if(randomWord.indexOf(keyWord) > -1) {
         rightWord.push(keyWord);
         underScore[randomWord.indexOf(keyWord)] = keyWord;
 }
+
 // check to see if user entries match randomWord      
 if(underScore.join('') == randomWord) {
         alert("Winner!!");
 }
+
 
 });
 
@@ -61,4 +63,5 @@ for (var i = 0; i < words.length; i++) {
 }
 
 var remainingLetters = words.length;
+
 
